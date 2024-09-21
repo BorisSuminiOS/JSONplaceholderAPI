f0ab89da2603e1dbfc268cbf3f9e726b9d0c87f1
Boris <MTK26@mail.ru>
2024-09-21 14:13:20 +0300

    Создание класса TestJsonPlaceholder и реализация метода test_get_users

---
+++ tests/test_json_placeholder.py
@@ -0,0 +1,13 @@
+from utilities.checking import Checking
+import allure
+from utilities.json_placeholder_api import JsonPlaceholderApi
+
+
+@allure.epic('Тестирование пользователей')
+class TestJsonPlaceholder:
+
+    @classmethod
+    @allure.description('Тест на отображение пользователей')
+    def test_get_users(cls):
+        response = JsonPlaceholderApi.get_users()
+        Checking.check_status_code(response, 200)
