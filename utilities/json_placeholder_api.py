5df018c0fa001227948cfd8afc5c1958e7b1124d
Boris <MTK26@mail.ru>
2024-09-21 13:50:35 +0300

    Реализация метода: get_users в модуле json_placeholder_api

---
+++ utilities/json_placeholder_api.py
@@ -0,0 +1,20 @@
+import requests
+from utilities.http_methods import Http_methods
+
+base_url = 'https://jsonplaceholder.typicode.com'
+
+
+class JsonPlaceholderApi:
+    '''API методы, create, read, update, delete users (CRUD)'''
+
+
+    @staticmethod
+    def get_users():
+        '''Показать список пользователей'''
+
+        resource_get = '/users'
+        url = f'{base_url}{resource_get}'
+        result_get = Http_methods.get(url)
+        print(f'Ответ от сервера: {result_get.text}')
+        print(f'Статус код: {result_get.status_code}')
+        return result_get
