7d2e93d711ba51fc1263c11cd41bdc5cc08ed29f
Boris <MTK26@mail.ru>
2024-09-21 15:46:32 +0300

    Реализация метода check_field_email

--- tests/test_json_placeholder.py
+++ tests/test_json_placeholder.py
@@ -3,19 +3,19 @@ import allure
 from utilities.json_placeholder_api import JsonPlaceholderApi
 
 
-
 @allure.epic('Тестирование пользователей')
 class TestJsonPlaceholder:
 
     @classmethod
     @allure.description('Тест на отображение пользователей')
     def test_get_users(cls):
-        list_custom_fields = ['id', 'name', 'username', 'email', 'address', 'phone', 'website', 'company']
         response = JsonPlaceholderApi.get_users()
+
         Checking.check_status_code(response, 200)
         Checking.check_list_names_users(response)
         Checking.check_list_users(response)
-        Checking.check_list_user_fields(response, list_custom_fields)
+        Checking.check_list_user_fields(response,
+        ['id', 'name', 'username', 'email', 'address', 'phone', 'website', 'company'])
         Checking.check_field_email(response)
 
 
