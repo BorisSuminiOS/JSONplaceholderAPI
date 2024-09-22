38b135e8c00e4309fe628eeb8d5e58e238167ed1
Boris <MTK26@mail.ru>
2024-09-22 16:01:06 +0300

    Редактирование кода

--- utilities/checking.py
+++ utilities/checking.py
@@ -3,7 +3,7 @@ from requests import Response
 
 
 class Checking:
-    '''Проверки ответов от сервера'''
+    """Проверки ответов от сервера"""
 
     @staticmethod
     def check_status_code(response: Response, status_code):
@@ -17,19 +17,18 @@ class Checking:
         assert required_fields_response == required_fields
         print(f"Обязательные поля: Все указанные поля в ответе от сервера присутствуют")
 
-
     @staticmethod
     def check_list_names_users(response: Response):
         result = response.json()
         names = [i.get('name') for i in result]
-        assert len(names) > 0
+        assert len(names) > 0, "Список имен пользователей пустой"
         print(f'Проверка списка имен пользователей: [{len(names)}] имен. Список не пустой')
 
     @staticmethod
     def check_list_users(response: Response):
         result = response.json()
         users = [i for i in result]
-        assert len(users) > 0
+        assert len(users) > 0, "Список пользователей пустой"
         print(f'Проверка списка пользователей: [{len(users)}] пользователей. Список не пустой')
 
     @staticmethod
@@ -37,15 +36,15 @@ class Checking:
         result = response.json()
         users = [[*i.keys()] for i in result]
         for k,v in enumerate(users):
-            assert required_fields == v
-            print(f"Проверка полей: У пользователя №{k + 1} все поля присутствуют \n {required_fields}")
+            assert required_fields == v, "Указанные поля не найдены"
+            print(f"Проверка полей: У пользователя №{k + 1} все поля присутствуют\n {required_fields}")
 
     @staticmethod
     def check_field_email(response: Response):
         result = response.json()
         email = [i.get('email') for i in result]
         for k,v in enumerate(email):
-            assert "@" in v
+            assert "@" in v, f"Указанный знак у пользователя {v} отсутствует"
             print(f"Проверка поле Email: У пользователя №{k + 1} в email присутствует символ '@'")
 
 
--- utilities/json_placeholder_api.py
+++ utilities/json_placeholder_api.py
@@ -4,16 +4,15 @@ base_url = 'https://jsonplaceholder.typicode.com'
 
 
 class JsonPlaceholderApi:
-    '''API методы, create, read, update, delete users (CRUD)'''
-
+    """API методы, create, read, update, delete users (CRUD)"""
 
     @staticmethod
     def get_users():
-        '''Показать список пользователей'''
+        """Показать список пользователей"""
 
         resource_get = '/users'
         url = f'{base_url}{resource_get}'
         result_get = Http_methods.get(url)
         print(f'Ответ от сервера: {result_get.text}')
-        print(f'Файтический статус код: {result_get.status_code}')
+        print(f'Фактический статус код: {result_get.status_code}')
         return result_get
