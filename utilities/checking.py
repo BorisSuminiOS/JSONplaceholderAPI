7c5920188cc1fc46e79b051db5945a4dead6f379
Boris <MTK26@mail.ru>
2024-09-22 16:07:48 +0300

    Редактирование кода

--- utilities/checking.py
+++ utilities/checking.py
@@ -8,13 +8,13 @@ class Checking:
     @staticmethod
     def check_status_code(response: Response, status_code):
         status_code_response = response.status_code
-        assert status_code_response == status_code
+        assert status_code_response == status_code,f"Ожидался статус-код {status_code}, но получен {status_code_response}"
         print(f'Проверка на статус код: Успешно [{status_code_response}]')
 
     @staticmethod
     def check_all_required_fields(response: Response, required_fields):
         required_fields_response = json.loads(response.text)
-        assert required_fields_response == required_fields
+        assert required_fields_response == required_fields, f'Указанные поля в ответе от сервера отсутствуют'
         print(f"Обязательные поля: Все указанные поля в ответе от сервера присутствуют")
 
     @staticmethod
